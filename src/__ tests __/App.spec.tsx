fd6204c5f9bc04c79d9dbab6bd7fadf3cdf4a978
FrahOrru <francesca.orru00@outlook.it>
2024-02-02 11:32:24 +0100

    eslint and pretter

--- src/__ tests __/App.spec.tsx
+++ src/__ tests __/App.spec.tsx
@@ -1,25 +1,26 @@
 import '@testing-library/jest-dom'
-import App from "../App"
-import { render, fireEvent } from '@testing-library/react';
+import App from '../App'
+import { render, fireEvent } from '@testing-library/react'
+import React from 'react'
 
 test('demo', () => {
-    expect(true).toBe(true)
+  expect(true).toBe(true)
 })
 
-test("Renders the main page", () => {
-    render(<App />)
-    expect(true).toBeTruthy()
+test('Renders the main page', () => {
+  render(<App />)
+  expect(true).toBeTruthy()
 })
 
 test('renders the counter button and updates count on click', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<App />)
 
   // Initial count is 0
-  expect(getByText(/count is 0/i)).toBeInTheDocument();
+  expect(getByText(/count is 0/i)).toBeInTheDocument()
 
   // Click the button
-  fireEvent.click(getByText(/count is 0/i));
+  fireEvent.click(getByText(/count is 0/i))
 
   // After clicking, count should be 1
-  expect(getByText(/count is 1/i)).toBeInTheDocument();
-});
+  expect(getByText(/count is 1/i)).toBeInTheDocument()
+})
