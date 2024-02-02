d865b3181204c31b56290aace75c45cd2dfccd3a
FrahOrru <francesca.orru00@outlook.it>
2024-02-02 10:41:24 +0100

    tests and first commit

---
+++ src/__ tests __/App.spec.tsx
@@ -0,0 +1,25 @@
+import '@testing-library/jest-dom'
+import App from "../App"
+import { render, fireEvent } from '@testing-library/react';
+
+test('demo', () => {
+    expect(true).toBe(true)
+})
+
+test("Renders the main page", () => {
+    render(<App />)
+    expect(true).toBeTruthy()
+})
+
+test('renders the counter button and updates count on click', () => {
+  const { getByText } = render(<App />);
+
+  // Initial count is 0
+  expect(getByText(/count is 0/i)).toBeInTheDocument();
+
+  // Click the button
+  fireEvent.click(getByText(/count is 0/i));
+
+  // After clicking, count should be 1
+  expect(getByText(/count is 1/i)).toBeInTheDocument();
+});
